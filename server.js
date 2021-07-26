f69591d04a3e58bd20fc4d3154b660da775cab9c
irfan <irfan.alhady25@gmail.com>
2021-07-27 01:30:10 +0700

    add start

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ app.post("/", (req, res)=>{
     res.sendFile(__dirname + "/Thankyou.html");
 });
 
-app.listen(8080, function(){
+app.listen(process.env.PORT || 8080, function(){
     console.log("server is running on 8080");
 })
